8177915391ea277d76b1bd272c52a1e7beced807
CptKappa <benjamin.luxbacher@gmail.com>
2018-08-15 18:22:28 +0200

    Added methods

--- IBMQuantumExperience/Request.cpp
+++ IBMQuantumExperience/Request.cpp
@@ -1,23 +1,60 @@
 // Copyright by Benjamin Luxbacher
 #include "Request.h"
 
-//def __init__(self, token, config = None, verify = True, retries = 5,
-//	timeout_interval = 1.0) :
-//	self.verify = verify
-//	self.client_application = CLIENT_APPLICATION
-//	self.config = config
-//
-//	# Set the proxy information, if present, from the configuration,
-//	# with the following format :
-//# config = {
-//#     'proxies': {
-//#         # If using 'urls', assume basic auth or no auth.
-//#         'urls': {
-//#             'http': 'http://user:password@1.2.3.4:5678',
-//#             'https': 'http://user:password@1.2.3.4:5678',
-//#         }
-//#         # If using 'ntlm', assume NTLM authentication.
-//#         'username_ntlm': 'domain\\username',
-//#         'password_ntlm': 'password'
-//#     }
-//# }
+// The Request class to manage the methods
+Request::Request(std::string token, std::map<std::string, std::string> config = std::map<std::string, std::string>(), bool verify = true, int retries = 5, double timeout_interval = 1.0)
+{
+
+}
+
+Request::~Request()
+{
+
+}
+
+// Check is the user's token is valid
+bool Request::CheckToken(int status_code)
+{
+
+}
+
+// POST Method Wrapper of the REST API
+std::map<std::string, std::string> Request::Post(std::string path, std::string params = "", std::string data = "")
+{
+
+}
+
+// PUT Method Wrapper of the REST API
+std::map<std::string, std::string> Request::Put(std::string path, std::string params = "", std::string data = "")
+{
+
+}
+
+// GET Method Wrapper of the REST API
+std::map<std::string, std::string> Request::Get(std::string path, std::string params = "", bool with_token = true)
+{
+
+}
+
+// check response
+// @param: std::string respond: HTTP response.
+// @return: bool: true if the response is good, else false.
+// @raises: ApiError: response isn't formatted properly.
+bool Request::ResponseGood(HTTPResponse respond)
+{
+
+}
+
+// parse text of response for HTTP errors
+/*
+    This parses the text of the response to decide whether to
+    retry request or raise exception. At the moment this only
+    detects an exception condition.
+*/
+// @param: Response respond: requests.Response object
+// @return: bool: false if the request should be retried, true if not.
+// @raises: RegisterSizeError
+bool Request::ParseResponse(HTTPResponse respond)
+{
+
+}
