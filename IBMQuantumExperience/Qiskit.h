286b493c3ea243c8253d18650be0ac02ed2b7229
CptKappa <benjamin.luxbacher@gmail.com>
2018-08-23 21:48:18 +0200

    Added split method

--- IBMQuantumExperience/Qiskit.h
+++ IBMQuantumExperience/Qiskit.h
@@ -3,6 +3,8 @@
 #define _QISKIT_H_
 
 #include <string>
+#include <vector>
+#include <algorithm>
 
 namespace Qiskit
 {
@@ -12,6 +14,27 @@ namespace Qiskit
     ? [] CleanQObjectResult(job);*/
 
     const std::string CLIENT_APPLICATION = "qiskit-api-cpp";
+
+    std::vector<std::string> split(std::string_view content, std::string_view delimiter)
+    {
+        std::vector<std::string> result;
+        auto prev_pos = content.begin();
+        auto next_pos = std::search(prev_pos, content.end(), delimiter.begin(), delimiter.end());
+
+        while (next_pos != content.end())
+        {
+            result.emplace_back(prev_pos, next_pos);
+            prev_pos = next_pos + delimiter.size();
+            next_pos = std::search(prev_pos, content.end(), delimiter.begin(), delimiter.end());
+        }
+
+        if (prev_pos != content.end())
+        {
+            result.emplace_back(prev_pos, content.end());
+        }
+
+        return result;
+    }
 }
 
 #endif
