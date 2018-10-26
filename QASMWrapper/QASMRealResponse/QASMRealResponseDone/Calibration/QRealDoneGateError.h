b04c02614f25d9e87e9f5083b6e6dbc9dc371c61
Masaru26 <jonas.hirsch@gmx.at>
2018-10-26 15:05:48 +0200

    QRealDoneGateError.h added constructor structure

--- QASMWrapper/QASMRealResponse/QASMRealResponseDone/Calibration/QRealDoneGateError.h
+++ QASMWrapper/QASMRealResponse/QASMRealResponseDone/Calibration/QRealDoneGateError.h
@@ -2,6 +2,9 @@
 
 class QRealDoneGateError{
     public:
+        QRealDoneGateError();
+        QRealDoneGateError(std::string, double);
+        
         std::string date;
         double value;
 };
