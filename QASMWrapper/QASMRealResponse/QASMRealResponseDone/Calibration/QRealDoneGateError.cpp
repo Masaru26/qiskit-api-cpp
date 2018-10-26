705fdecfa6692155f790a3d066206b5137c9349b
Masaru26 <jonas.hirsch@gmx.at>
2018-10-26 15:07:03 +0200

    QRealDoneGateError.cpp added constructors

---
+++ QASMWrapper/QASMRealResponse/QASMRealResponseDone/Calibration/QRealDoneGateError.cpp
@@ -0,0 +1,8 @@
+#include "QRealDoneGateError.h"
+
+QRealDoneGateError::QRealDoneGateError(){};
+
+QRealDoneGateError::QRealDoneGateError(std::string date, double value){
+      QRealDoneGateError::date = date;
+      QRealDoneGateError::value = value;
+};
