82e02673e239f7f41f797668ef5c95f801af7429
Masaru26 <jonas.hirsch@gmx.at>
2018-10-26 15:08:26 +0200

    QRealDoneFridgeTemperature.h added constructor structure

--- QASMWrapper/QASMRealResponse/QASMRealResponseDone/Calibration/QRealDoneFridgeTemperature.h
+++ QASMWrapper/QASMRealResponse/QASMRealResponseDone/Calibration/QRealDoneFridgeTemperature.h
@@ -2,6 +2,9 @@
 #include <list>
 class QRealDoneFridgeTemperature{
     public:
+        QRealDoneFridgeTemperature();
+        QRealDoneFridgeTemperature(std::string, std::list<std::string>, std::string);
+    
         std::string date;
         std::list<std::string> value;
         std::string unit; 
